f81911a4777413314162382d4acc702e2ea9ccb7
Chinecherem <chinecheremubawike@gmail.com>
2023-11-10 14:04:59 +0100

    neche

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Tutorial from './toutorial/tourials';
 //A change
+//by neche
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
