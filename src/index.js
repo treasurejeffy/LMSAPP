42eebeea6508eacc4acab1d66414e0a15e76f617
Chinecherem <chinecheremubawike@gmail.com>
2023-11-10 14:00:56 +0100

    xyz

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Tutorial from './toutorial/tourials';
+//A change
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
