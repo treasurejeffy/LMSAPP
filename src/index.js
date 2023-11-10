019436942b73ecbbc4c2318908a704f09bac08cc
treasurejeffy <treasurejeffy@gmail.com>
2023-11-10 14:04:49 +0100

    small changes made

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Tutorial from './toutorial/tourials';
 //A change
-
+//by treasure
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Tutorial/>
